e9535b62e92ef927c252fb447ae752cc7b06d8c2
Rafael Kitromilidis <130234454+kitros03@users.noreply.github.com>
2025-05-12 17:51:01 +0300

    Create main.java

---
+++ src/main.java
@@ -0,0 +1,12 @@
+public class main{
+    public static void main(String[] args){
+
+        Services serv = new Services();
+        Cart cart =  new Cart();
+        User user = new User();
+        Business_User b_user =new Business_User();
+        Find_Gym fgym = new Find_Gym();
+        Menu menu = new Menu();
+        
+    }
+}
