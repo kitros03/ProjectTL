2aca093f471048689195bd9a4bc82367f49129c8
Rafael Kitromilidis <130234454+kitros03@users.noreply.github.com>
2025-05-12 18:29:35 +0300

    changes!

---
+++ src/Favourites.java
@@ -0,0 +1,9 @@
+public class Favourites extends Services {
+    private String service_name;
+    private float service_price;
+    private String service_category;
+    private int service_id;
+    private int business_id;
+    private int user_id;
+    private String business_name;
+}
