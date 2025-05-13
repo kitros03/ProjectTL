50c55e115efdc9cced41d727fa88c5fb3551ce9b
ARootK02 <akbk939@gmail.com>
2025-05-13 16:40:22 +0300

    S

--- src/main.java
+++ src/main.java
@@ -1,10 +1,10 @@
-public class main{
+public class Main{
     public static void main(String[] args){
 
         Services serv = new Services();
         Cart cart =  new Cart();
         User user = new User();
-        Company_User c_user =new Company_User();
+        Company_User b_user =new Company_User();
         Find_Gyms fgym = new Find_Gyms();
         Menu menu = new Menu();
         
