db8baea85a1dd95658d1511e1af0eb479b8e57cf
ARootK02 <akbk939@gmail.com>
2025-05-30 13:43:30 +0300

    Delete Subscription_History.java

--- src/Subscription_History.java
+++
@@ -1,9 +0,0 @@
-public class Subscription_History extends Order{
-
-    public Subscription_History(int company_id, int user_id) {
-        super(company_id, user_id);
-    }   
-
-    public void getSubscriptionHistory(int user_id) {
-    }
-}
